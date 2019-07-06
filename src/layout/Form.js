a6b91d220c3429ba016c70ed10e8f614bc7b04f2
Dayshawn Terry <Dayshawn21@gmail.com>
2019-07-06 16:26:16 -0500

    5 commit

--- src/layout/Form.js
+++ src/layout/Form.js
@@ -9,9 +9,30 @@ const Form = () => {
 	return (
 		<div className="container">
 			<h1 className="large text-center"> How Can I Help You </h1>
-			<form onSubmit={handleSubmit} className="form" name="contact" method="POST" data-netlify="true">
-				<input type="text" name="name" placeholder="Name" value={values.name} onChange={handleChange} />
-				<input type="email" name="email" placeholder="Email " value={values.email} onChange={handleChange} />
+			<form
+				onSubmit={handleSubmit}
+				className="form"
+				name="contact"
+				method="POST"
+				netlify-honeypot="bot-field"
+				data-netlify="true"
+			>
+				<input
+					type="text"
+					name="name"
+					placeholder="Name"
+					value={values.name}
+					onChange={handleChange}
+					required
+				/>
+				<input
+					type="email"
+					name="email"
+					placeholder="Email "
+					value={values.email}
+					onChange={handleChange}
+					required
+				/>
 				<textarea
 					name="textarea"
 					id=""
@@ -23,7 +44,9 @@ const Form = () => {
 				<div className="field">
 					<div data-netlify-recaptacha="true" />
 				</div>
-				<input type="submit" value="Search" className="btn btn-dark btn-block" />
+				<button type="submit" value="Search" className="btn btn-dark btn-block">
+					Send
+				</button>
 			</form>
 		</div>
 	);
