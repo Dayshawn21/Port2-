5a1e17e5f2f38dec533e6343770a11f1a22fc0dd
Dayshawn Terry <Dayshawn21@gmail.com>
2019-07-07 22:06:59 -0500

    8 commit

--- src/layout/Form.js
+++ src/layout/Form.js
@@ -1,51 +1,38 @@
 import React from 'react';
-import useForm from './useForm';
+import NetlifyForm from 'react-netlify-form';
 
 const Form = () => {
-	const { values, handleChange, handleSubmit } = useForm();
-
 	return (
 		<div className="container">
 			<h1 className="large text-center"> How Can I Help You </h1>
-			<form
-				onSubmit={handleSubmit}
-				className="form"
-				name="contact"
-				method="POST"
-				netlify-honeypot="bot-field"
-				data-netlify="true"
-			>
-				<input
-					type="text"
-					name="name"
-					placeholder="Name"
-					value={values.name}
-					onChange={handleChange}
-					required
-				/>
-				<input
-					type="email"
-					name="email"
-					placeholder="Email "
-					value={values.email}
-					onChange={handleChange}
-					required
-				/>
-				<textarea
-					name="textarea"
-					id=""
-					cols="30"
-					rows="10"
-					placeholder="How Can I Help You "
-					onChange={handleChange}
-				/>
-				<div className="field">
-					<div data-netlify-recaptacha="true" />
-				</div>
-				<button type="submit" value="Search" className="btn btn-dark btn-block">
-					Send
-				</button>
-			</form>
+			<NetlifyForm name="contact" canSubmit="true">
+				{({ loading, error, success }) => (
+					<div>
+						{loading && <div>Loading...</div>}
+						{error && <div>Your information was not sent. Please try again later.</div>}
+						{success && <div>Thank you for contacting us!</div>}
+						{!loading &&
+						!success && (
+							<div>
+								<input type="hidden" name="contact" value="contact" />
+								<input type="text" name="name" placeholder="Name" required />
+								<input type="email" name="email" placeholder="Email" required />
+								<textarea
+									type="textarea"
+									name="message"
+									cols="30"
+									rows="10"
+									placeholder="How can I help you"
+									required
+								/>
+								<button type="submit" className="btn btn-dark btn-block">
+									Submit
+								</button>
+							</div>
+						)}
+					</div>
+				)}
+			</NetlifyForm>
 		</div>
 	);
 };
